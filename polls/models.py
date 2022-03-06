54bbbea4c56d95a49a233ac411d41bf7e3d04fe1
jsdnlb <danielb.alzate1@gmail.com>
2022-03-06 17:33:44 -0500

    Change polls models using method __str__

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # After programming the classes we must migrate them with the following commands
@@ -7,13 +9,22 @@ from django.db import models
 
 
 class Question(models.Model):
-    # id  I commend id because
+    # id  I commend id because is not neccesary
     question_text = models.CharField(max_length=200)
     issue_date = models.DateTimeField("issue date")
-    auto_date = models.DateTimeField(auto_now_add = True)
+    auto_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + ': ' + self.question_text
+
+    def was_published_recently(self):
+        return self.issue_date >= timezone.now - datetime.timedelta(days=1)
 
 
-class Choices(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
--- popularitypollapp/settings.py
+++ popularitypollapp/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-DJANGO_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
 
 LOCAL_APPS = (
     'polls',
