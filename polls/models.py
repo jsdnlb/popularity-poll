9e33fc39d1692f24e615cb4f9ecc806ed38c671f
jsdnlb <danielb.alzate1@gmail.com>
2022-03-06 17:00:43 -0500

    Adding migrations model polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# After programming the classes we must migrate them with the following commands
+# python3 manage.py makemigrations polls
+# python3 manage.py migrate
+
+
+class Question(models.Model):
+    # id  I commend id because
+    question_text = models.CharField(max_length=200)
+    issue_date = models.DateTimeField("issue date")
+    auto_date = models.DateTimeField(auto_now_add = True)
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- popularitypollapp/settings.py
+++ popularitypollapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = (
+    'polls',
+)
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
