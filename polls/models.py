8a112bc19c8795aea9cd63f6ee42f16e25f54999
jsdnlb <danielb.alzate1@gmail.com>
2022-03-07 15:01:06 -0500

    Using commands in shell

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,22 @@ from django.utils import timezone
 # python3 manage.py makemigrations polls
 # python3 manage.py migrate
 
+"""
+Using in shell of Python
+Documentation: https://docs.djangoproject.com/en/3.2/topics/db/queries/#field-lookups-intro
+
+ __gt = Greater than
+__gte = Greater than or equal to
+__lt = Less than
+__lte = Less than or equal to
+__startswith = Starts with
+__endswith = Ends with
+__exact: A case-insensitive match.
+contains: Contains
+__icontains: Case-insensitive version of startswith.
+__istartswith = case-insensitive version of startswith
+__iendswith = case-insensitive version of endswith
+ """
 
 class Question(models.Model):
     # id  I commend id because is not neccesary
