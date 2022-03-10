9d6c5212afb908175a915136ae18ae4a031c8307
jsdnlb <danielb.alzate1@gmail.com>
2022-03-10 08:40:26 -0500

    Creating simple views from url

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    # ex: /polls/1
+    path('<int:question_id>/',views.detail, name='index'),
+    # ex: /polls/1/result
+    path('<int:question_id>/result',views.result, name='index'),
+    # ex: /polls/1/vote
+    path('<int:question_id>/vote',views.vote, name='index')
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('You are on the main page Popularity Poll')
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking question number {question_id}')
+
+def result(request, question_id):
+    return HttpResponse(f'You are looking result of the question number {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting question number {question_id}')
