294ef419dfed5591b6222c1b6dace2b76367d7bf
jsdnlb <danielb.alzate1@gmail.com>
2022-03-17 17:50:47 -0500

    Create template index in polls

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('You are on the main page Popularity Poll')
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {
+        'latest_question_list': latest_question_list
+    })
+
 
 def detail(request, question_id):
     return HttpResponse(f'You are looking question number {question_id}')
 
+
 def result(request, question_id):
     return HttpResponse(f'You are looking result of the question number {question_id}')
+
 
 def vote(request, question_id):
     return HttpResponse(f'You are voting question number {question_id}')
