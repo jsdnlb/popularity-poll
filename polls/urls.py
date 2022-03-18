a0bf5db36012786a434c4f3c8eb0d129a504f415
jsdnlb <danielb.alzate1@gmail.com>
2022-03-18 11:44:49 -0500

    Using url tags

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 # Import views from polls
 from . import views
-
-urlpatterns= [
-    path('',views.index, name='index'),
+app_name = 'polls'
+urlpatterns = [
+    path('', views.index, name='index'),
     # ex: /polls/1
-    path('<int:question_id>/',views.detail, name='index'),
+    path('<int:question_id>/detail/examples/dani/jsdnlb/',
+         views.detail, name='detail'),
     # ex: /polls/1/result
-    path('<int:question_id>/result',views.result, name='index'),
+    path('<int:question_id>/result', views.result, name='result'),
     # ex: /polls/1/vote
-    path('<int:question_id>/vote',views.vote, name='index')
+    path('<int:question_id>/vote', views.vote, name='vote')
 ]
